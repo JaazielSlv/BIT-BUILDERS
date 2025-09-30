a7bf2679d37192256f5f712b3b3e6eba06bbf752
JaazielSlv <Jaaziel.b.silva@gmail.com>
2025-09-30 20:26:46 -0300

    30-09

--- src/ficha_tecnica/script.js
+++ src/ficha_tecnica/script.js
@@ -2,13 +2,13 @@ let currentImageIndex = 0;
 const images = document.querySelectorAll('.carousel-image');
 
 function showNextImage() {
-    images[currentImageIndex].style.opacity = 0; // Esconde a imagem atual
-    currentImageIndex = (currentImageIndex + 1) % images.length; // Incrementa o índice
-    images[currentImageIndex].style.opacity = 1; // Mostra a próxima imagem
+    images[currentImageIndex].style.opacity = 0;
+    currentImageIndex = (currentImageIndex + 1) % images.length; 
+    images[currentImageIndex].style.opacity = 1; 
 }
 
-// Inicia o loop para alternar as imagens a cada 5 segundos (5000ms)
+
 setInterval(showNextImage, 5000);
 
-// Exibe a primeira imagem
+
 images[currentImageIndex].style.opacity = 1;
